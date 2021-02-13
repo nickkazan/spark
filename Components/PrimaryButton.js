d8f25a3d11a135d79d13b6f0c12b7ecc8a2bdfbb
Nick Kazan <nicholas.kazan1@gmail.com>
2021-02-13 13:57:54 -0500

    added two sections where you choose categories and prices

--- Components/PrimaryButton.js
+++ Components/PrimaryButton.js
@@ -2,26 +2,26 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const StyledTouchable = styled.TouchableOpacity`
-    flex: 1;
-    background-color: #fff;
+    background-color: #2a9d8f;
     align-items: center;
+    width: 75%;
     justify-content: center;
     border-radius: 10px;
+    margin-bottom: 50px;
     padding-top: 20px;
     padding-bottom: 20px;
-    margin-left: 10px;
-    margin-right: 10px;
 `
 
 const StyledText = styled.Text`
   font-family: 'Avenir';
   font-size: 18px;
+  color: #fff;
 `
 
-export default function Choice(props) {
+export default function PrimaryButton(props) {
   return (
-    <StyledTouchable onPress={props.onPress} style={props.style}>
-      <StyledText style={props.textStyle}>{props.name}</StyledText>
+    <StyledTouchable onPress={props.onPress}>
+      <StyledText> {props.text} </StyledText>
     </StyledTouchable>
   )
 }
--- Pages/Home.js
+++ Pages/Home.js
@@ -1,37 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
 import styled from 'styled-components/native';
 
 import Choice from '../Components/Choice';
+import PrimaryButton from '../Components/PrimaryButton';
 
 const StyledContainer = styled.View`
-  flex: 1;
+  flex: 10;
   background-color: #fff;
   align-items: center;
   justify-content: center;
 `
 
 const StyledRow = styled.View`
-  flex: 1;
+  flex: 8;
   flex-direction: row;
   justify-content: center;
   align-items: center;
 `
 
 export default function Home() {
-  const [listOfChoices, setListOfChoices] = useState([
-      {choiceName: 'Restaurants', 'isActive': false},
-      {choiceName: 'Sports', 'isActive': false},
-      {choiceName: 'Bars', 'isActive': true}
-  ])
+  const listOfCategories = [
+    {choiceName: 'Restaurants', 'isActive': false},
+    {choiceName: 'Sports', 'isActive': false},
+    {choiceName: 'Bars', 'isActive': false}
+  ]
+  const listOfPrices = [
+    {choiceName: '$', 'isActive': false},
+    {choiceName: '$$', 'isActive': false},
+    {choiceName: '$$$+', 'isActive': false}
+  ]
+  const titles = [['categories', listOfCategories], ['prices', listOfPrices]]
 
-  const selectChoice = (choice, index) => {
-    console.log(listOfChoices)
+  const [listOfChoices, setListOfChoices] = useState(listOfCategories)
+  const [choiceCounter, setChoiceCounter] = useState(0)
+  const [finalChoices, setFinalChoices] = useState({})
+
+  useEffect(() => {
+    if (choiceCounter < titles.length) {
+      setListOfChoices(titles[choiceCounter][1])
+    }
+  }, [choiceCounter])
+
+  useEffect(() => {
+    console.log("Final is now: ", finalChoices)
+  }, [finalChoices])
+
+  const selectChoice = (index) => {
+    // console.log(listOfChoices)
     var tempListOfChoices = listOfChoices.slice()
     tempListOfChoices[index].isActive = !tempListOfChoices[index].isActive
     setListOfChoices(tempListOfChoices)
-    console.log(listOfChoices)
+  }
+
+  const submitChoices = (finalListOfChoices) => {
+    console.log("Choice Counter Length: ", choiceCounter)
+    if (choiceCounter < titles.length) {
+      var choicesWithActiveStatus = []
+      finalListOfChoices.forEach((choice) => {
+        if (choice.isActive) {
+          choicesWithActiveStatus.push(choice.choiceName)
+        }
+      })
+      console.log(choiceCounter)
+      console.log(titles.length)
+      console.log(titles[choiceCounter][0])
+      var keyName = titles[choiceCounter][0]
+  
+      setFinalChoices({...finalChoices, [keyName]: choicesWithActiveStatus})
+      setChoiceCounter(choiceCounter + 1)
+    } else {
+      console.log("Our Choice Counter is passed the limit.")
+    }
   }
 
   const inactiveChoiceStyle = {
@@ -52,10 +91,11 @@ export default function Home() {
     <StyledContainer>
       <StyledRow>
         { listOfChoices.map((item, index) => (
-          <Choice key={index} name={item.choiceName} textStyle={ item.isActive ? activeChoiceStyle: inactiveChoiceStyle } style={ item.isActive ? activeChoiceStyle: inactiveChoiceStyle } onPress={() => selectChoice(item, index)} />
+          <Choice key={index} name={item.choiceName} textStyle={ item.isActive ? activeTextStyle: inactiveTextStyle } style={ item.isActive ? activeChoiceStyle: inactiveChoiceStyle } onPress={() => selectChoice(index)} />
         )) 
         }
       </StyledRow>
+      <PrimaryButton text="Next" onPress={() => submitChoices(listOfChoices)}/>
     </StyledContainer>
   )
 }
