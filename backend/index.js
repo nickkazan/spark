69c56aa57f5b27151f5840a238f8838ab686f6e9
Nick Kazan <nicholas.kazan1@gmail.com>
2021-02-27 14:36:41 -0500

    added yelp API and formalized the request to it

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,42 @@
 const express = require("express")
+require("dotenv").config()
+const ck = require("ckey");
+const axios = require('axios');
 
 const PORT = 8080
+const URL =  "https://api.yelp.com/v3/businesses/search"
+const API_KEY = ck.YELP_API_KEY
+
 const app = express()
 app.use(express.json());
 
 app.post("/resulting-activities", (req, res) => {
   const body = req.body
   console.log(body)
+  const rawParams = {
+    'location': '219 Clarence Street, Ottawa, ON K1N 5R2', // MUST BE USER DETERMINED
+    'radius': Math.max(...body.transportation),
+    'categories': body.categories.toString(),
+    'limit': 5,
+    'price': body.prices.toString(),
+    'open_now': true // MUST BE USER DETERMINED
+  }
+
+  var params = new URLSearchParams(rawParams)
+  console.log(params)
+
+  axios.get(URL + "?" + params, {
+    headers: {
+      'Authorization': 'Bearer ' + API_KEY,
+      'Content-Type': 'application/json'
+    },
+  })
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((err) => {
+    console.error(err.response)
+  })
   res.send("Hello world");
 });
 
